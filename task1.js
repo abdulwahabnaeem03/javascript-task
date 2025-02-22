a75a466274defe090517ee3fcc654896dfd03a83
abdulwahabnaeem03 <awahabnaeem5@gmail.com>
2025-02-21 18:28:06 -0800

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,6 @@
+alert("Assalam u Alikum");
+    alert("Error! Please Enter A Valid Password");
+    alert("Welcome to js land...\n Happy coding" );
+    alert("Welcome to JS land...");
+    alert("Happy coding!")
+    alert("Hello... I can run JS through my web browser's console");
